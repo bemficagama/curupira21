03cab983faafa13b79e97ac576f25a890bfd7eba
Andre Bemfica <andre@microben.com.br>
2021-07-13 12:05:45 -0400

    key and url has category

--- src/backend/migrations/20210711170832_create_key_has_categories_table.js
+++ src/backend/migrations/20210711170832_create_key_has_categories_table.js
@@ -1,11 +1,11 @@
-
-exports.up = function (knex) {
-    return knex.schema.createTable('urls', table => {
-        table.increments('key_id').primary()
-        table.string('url').notNull()
+exports.up = function(knex) {
+    return knex.schema.createTable('key_has_categories', table => {
+        table.integer('key_id').unsigned().references('id').inTable('keys')
+        table.integer('category_id').unsigned().references('id').inTable('categories')
+        table.primary(['key_id', 'category_id'])
     })
 };
 
-exports.down = function (knex) {
-    return knex.schema.dropTable('urls')
+exports.down = function(knex) {
+    return knex.schema.dropTable('key_has_categories')
 };
--- src/backend/migrations/20210711170842_create_url_has_categories_table.js
+++ src/backend/migrations/20210711170842_create_url_has_categories_table.js
@@ -1,8 +1,11 @@
-
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('url_has_categories', table => {
+        table.integer('url_id').unsigned().references('id').inTable('urls')
+        table.integer('category_id').unsigned().references('id').inTable('categories')
+        table.primary(['url_id', 'category_id'])
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTable('url_has_categories')
 };
