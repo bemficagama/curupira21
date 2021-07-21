77655e957ef06ac70a66df42aa708cbd1348ed9e
Andre Bemfica <andre@microben.com.br>
2021-07-21 12:26:38 -0400

    frontjs

--- src/frontjs/karma.conf.js
+++ src/frontjs/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/SportsStore'),
+      dir: require('path').join(__dirname, './coverage/frontjs'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/frontjs/src/app/model/category.model.ts
+++
@@ -1,9 +0,0 @@
-export class Category {
-
-    constructor(
-        public id?: number,
-        public name?: string,
-        public category?: string,
-        public description?: string,
-        public parentId?: number) { }
-}
